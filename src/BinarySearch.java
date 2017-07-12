968fdb66272ba1639822788289a518a02ba0410f
bhawani0642 <bhawani0642@gmail.com>
2017-07-12 08:00:01 +0530

    Update BinarySearch.java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,47 +1,44 @@
-//Here we are implementing the Scanner class
 import java.util.Scanner;
-/*
- * Created a class named as BinarySearch
- */
 public class BinarySearch {
-//this is the main method
-	public static void main(String[] args) {
- int a, first,last,middle,n,search,array[];
- Scanner in= new Scanner(System.in);
- //print statement
- System.out.println("Please enter the number of elements");
-  n=in.nextInt();
-  array = new int[n];
-  System.out.println("Enter " + n + " integers");
-  //Running the for loop
-  for (a = 0; a < n; a++)
-      array[a] = in.nextInt();
-  //print statement
-  System.out.println("Enter value to find");
-  search = in.nextInt();
-  first  = 0;
-  last   = n - 1;
-  middle = (first + last)/2;
-
-  while( first <= last )
-  {if ( array[middle] < search )
-      first = middle + 1;    
-    else if ( array[middle] == search ) 
-    {
-      System.out.println(search + " found at location " + (middle + 1) + ".");
-      break;
-    }
-    else
-       last = middle - 1;
-
-    middle = (first + last)/2;
- }
- if ( first > last )
-    System.out.println(search + " is not present in the list.\n");
- {
+public static void main(String[] args) {
+  // Declaring variables
+  int num;
+  // Scanner object is used to get the inputs entered by the user
+  Scanner sc = new Scanner(System.in);
+  // Declaring one dimensional array
+  int arr[] = {4,   5,   6,   7,   12,   23,   34,   56,   78,   90,   123,   124,   156,   178,   199,   200,   230,   245,   250, 255};
+  // getting the number entered by the user
+  System.out.print("Enter the number to search :");
+  num = sc.nextInt();
+  // calling the method by passing the array and user entered number as
+  // arguments
+  int pos = searchForElement(arr, num);
+  // Displaying the position of the number in the array if found
+  if (pos != -1)
+   System.out.println("The " + num + " is found at Position " + pos);
+  else
+   System.out.println("** Number not in array **");
+}
+/*
+  * method implementation which search for the number in the array using
+  * binary search algorithm
+  * 
+  * @Params array and number
+  * 
+  * @return Position
+  */
+private static int searchForElement(int[] arr, int num) {
+  int first = 0, mid = 0;
+  int last = arr.length - 1;
+  while (first <= last) {
+   mid = (first + last) / 2;
+   if (num == arr[mid])
+    return mid;
+   if (num < arr[mid])
+    last = mid - 1;
+   else
+    first = mid + 1;
+  }
+  return -1;
 }
 }
-
-	}
-
-
